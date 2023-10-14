1b933167d2bc19168a58a2f728fe4f9d60ca5f41
ogornakevich <147474699+ogornakevich@users.noreply.github.com>
2023-10-14 20:26:15 +0300

    first

---
+++ АП_КН-104Б_Горнякевич_ЛР-02.1/PR1/labs2.1.cpp
@@ -0,0 +1,26 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
+
+
+using namespace std;
+int main()
+{   
+	
+	double x, z1, z2, PI;
+	cout << "x = "; cin >> x;
+	// 1 р-ня
+	z1 = cos(x) + sin(x) + cos(3 * x) + sin(3 * x);
+	// 2 р-ня
+	z2 = 2 * sqrt(2)*cos(x)*sin((M_PI/ 4) + 2 * x);
+	cout << "z1 = " << z1;
+	cout << " z2 = " << z2;
+
+}
+
+
