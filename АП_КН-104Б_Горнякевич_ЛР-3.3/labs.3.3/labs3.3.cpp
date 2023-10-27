606083402c3583212125fd6a293e5178aeb08ac7
ogornakevich <147474699+ogornakevich@users.noreply.github.com>
2023-10-27 17:06:01 +0300

    labs 3.3 / 27.10.2023

---
+++ АП_КН-104Б_Горнякевич_ЛР-3.3/labs.3.3/labs3.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+	double x, y;
+
+	cout << "x = "; cin >> x;
+
+	if (x <= -4)
+		y = -4;
+	else
+		if (-4 < x && x <= 0)
+			y = x / 4;
+		else
+			if (0 < x && x <= 2)
+				y = pow(x, 2);
+			else
+				y = -(x / 2) + 5;
+
+	cout << endl;
+	cout << "y = " << y << endl;
+
+	cin.get();
+	return 0;
+}
